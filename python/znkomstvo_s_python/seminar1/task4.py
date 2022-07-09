7a65ba7d3130d73799adf62ec8272893bb4af3f3
po100lit <po100lit@mail.ru>
2022-07-09 14:21:25 +0600

    refactor some code seminar1

--- python/znkomstvo_s_python/seminar1/task4.py
+++ python/znkomstvo_s_python/seminar1/task4.py
@@ -1,15 +1,13 @@
 # 2. Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа.
-#
 # *Примеры: *
-#
 # 6,78 -> 7
 # 5 -> нет
 # 0,34 -> 3
 
 def main():
     num = float(input('Enter number: '))
-    res = int(num*10%10)
-    if num-int(num) == 0:
+    res = int(num * 10 % 10)
+    if num - int(num) == 0:
         print('No')
     else:
         print(res)
