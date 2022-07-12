d668bb4814792f9bfa581156422804da07ed01d5
po100lit <po100lit@mail.ru>
2022-07-12 19:07:31 +0600

    add homework seminar2

--- python/znkomstvo_s_python/seminar2/homework/task4.py
+++ python/znkomstvo_s_python/seminar2/homework/task4.py
@@ -1,9 +1,17 @@
 # 4 - Реализуйте выдачу случайного числа (или алгоритм перемешивания списка)
 # не использовать random.randint и вообще библиотеку random.
 # Можете использовать xor, биты, библиотеку time (миллисекунды или наносекунды) - для задания случайности
+import time
+
+
+def random_number(from_number, to_number):
+    now = str(time.time())
+    rnd = float(now[::-1][:3]) / 1000
+    return int(from_number + rnd * (to_number - from_number))
+
 
 def main():
-    print('Hi!')
+    print(random_number(100, 1000))
 
 
 if __name__ == "__main__":
