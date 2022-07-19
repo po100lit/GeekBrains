e4693b3fadcd87683d720610bda583a9d8d4c9f7
po100lit <po100lit@mail.ru>
2022-07-19 13:52:59 +0600

    update task 3 homework seminar 4

--- python/znkomstvo_s_python/seminar4/homework/task3.py
+++ python/znkomstvo_s_python/seminar4/homework/task3.py
@@ -7,7 +7,7 @@ number = random.randint(2, 10000000)
 
 
 def is_prime(num: int) -> bool:  # проверка числа на простоту
-    for i in range(2, int(num ** 0.5 + 1)):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
@@ -15,17 +15,18 @@ def is_prime(num: int) -> bool:  # проверка числа на просто
 
 def get_prime_divisors(num: int) -> list:  # поиск простых делителей числа
     prime_divisors = []
-    for i in range(2, int(num // 2 + 1)):
+    for i in range(2, int(num // 2) + 1):
         if num % i == 0 and is_prime(i):
             prime_divisors.append(i)
-    if len(prime_divisors) == 0:
-        print('Число простое... простые делители единица и само число')
     return prime_divisors
 
 
 def main():
     print(f'Исходное число: {number}')
-    print(f'Простые делители:', ', '.join(list(map(str, get_prime_divisors(number)))))
+    if len(get_prime_divisors(number)) == 0:
+        print('Число простое... простые делители единица и само число')
+    else:
+        print(f'Простые делители:', ', '.join(list(map(str, get_prime_divisors(number)))))
 
 
 if __name__ == "__main__":
