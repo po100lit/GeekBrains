7601f3853498af2aa3f90fb91509eeed72f6d5ae
po100lit <po100lit@mail.ru>
2022-07-19 18:06:08 +0600

    update task 4 homework seminar 4

--- python/znkomstvo_s_python/seminar4/homework/task4.py
+++ python/znkomstvo_s_python/seminar4/homework/task4.py
@@ -2,7 +2,13 @@
 # Например:
 # Мама сшила м0не штаны и7з бере9зовой кор45ы 893. -> Мама сшила штаны.
 
-input_string = 'Мама сшила м0не штаны и7з бере9зовой кор45ы 893'
+def read_string(filepath: str) -> str:
+    with open(f'{filepath}', 'r', encoding='utf-8') as file:
+        string = file.read()
+        return string
+
+
+input_string = read_string('task4.txt')
 print(input_string)
 
 
@@ -15,8 +21,17 @@ def remove_words_with_digit(string: str) -> str:
     return ' '.join(result)
 
 
+output_string = remove_words_with_digit(input_string)
+
+
+def save_file(filepath: str):
+    with open(f'{filepath}', 'w', encoding='utf-8') as file:
+        file.write(output_string)
+
+
 def main():
-    print(remove_words_with_digit(input_string))
+    print(output_string)
+    save_file('new_task4.txt')
 
 
 if __name__ == "__main__":
