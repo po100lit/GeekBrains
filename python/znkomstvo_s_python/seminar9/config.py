dd780c7e6cbb6f7dc2acbbdf8cee291e5c128543
po100lit <po100lit@mail.ru>
2022-08-05 01:16:03 +0600

    update seminar9

---
+++ python/znkomstvo_s_python/seminar9/config.py
@@ -0,0 +1,8 @@
+TOKEN = '5238225700:AAGo86MOfTM2f75gOMKr2Q1jzjcL65SwCfM'
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/znkomstvo_s_python/seminar9/filik.py
@@ -0,0 +1,56 @@
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+
+def add(update, context):
+    arg = context.args
+    try:
+        res = sum(map(int, arg))
+        context.bot.send_message(update.effective_chat.id, f'{" + ".join(map(str, arg))} = {res}')
+    except ValueError:
+        context.bot.send_message(update.effective_chat.id, 'Enter numbers')
+
+
+def start(update, context):
+    arg = context.args
+    if not arg:
+        context.bot.send_message(update.effective_chat.id, "Привет")
+    else:
+        context.bot.send_message(update.effective_chat.id, f"{' '.join(arg)}")
+
+
+def info(update, context):
+    context.bot.send_message(update.effective_chat.id, "I'm Batman")
+
+
+def message(update, context):
+    text = update.message.text
+    if text.lower() == 'привет':
+        context.bot.send_message(update.effective_chat.id, 'И тебе привет..')
+    else:
+        context.bot.send_message(update.effective_chat.id, 'я тебя не понимаю')
+
+
+def unknown(update, context):
+    context.bot.send_message(update.effective_chat.id, f'Шо сказал, не пойму')
+
+add_handler = CommandHandler('add', add)
+start_handler = CommandHandler('start', start)  # /start фразочка
+info_handler = CommandHandler('info', info)  # /info
+message_handler = MessageHandler(Filters.text, message)
+unknown_handler = MessageHandler(Filters.command, unknown)  # /game
+
+dispatcher.add_handler(add_handler)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(info_handler)
+dispatcher.add_handler(unknown_handler)
+dispatcher.add_handler(message_handler)
+
+print('server started')
+updater.start_polling()
+updater.idle()
