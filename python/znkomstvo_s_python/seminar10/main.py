b7e41f7aee5ad7c38a9a12a341b93a618ab39e03
po100lit <po100lit@mail.ru>
2022-08-12 07:00:01 +0600

    обновил seminar10

--- python/znkomstvo_s_python/seminar10/main.py
+++ python/znkomstvo_s_python/seminar10/main.py
@@ -48,21 +48,22 @@ def take_move(update, context):
             candy_count -= take_candies
         else:
             context.bot.send_message(update.effective_chat.id, f'За раз можно взять {max_candy_takes} конфет')
+            # TODO: пропуск хода если не верное количество конфет
     except:
         context.bot.send_message(update.effective_chat.id, f'Введите число')
         return
     context.bot.send_message(update.effective_chat.id, f'На столе осталось {candy_count} конфет...')
     if candy_count < max_candy_takes:
         context.bot.send_message(update.effective_chat.id, f'Победил {current_player}')
+        # TODO: не правильный выигрыш
         return ConversationHandler.END
     current_player = player_2 if current_player == player_1 else player_1
     bot_move(update, context)
     if candy_count < max_candy_takes:
         context.bot.send_message(update.effective_chat.id, f'Победил {current_player}')
+        # TODO: не правильный выигрыш
         return ConversationHandler.END
 
-# TODO: пропуск хода если не верное количество конфет
-# TODO: не правильный выигрыш
 
 def bot_move(update, context):
     global candy_count, current_player
@@ -76,35 +77,11 @@ def bot_move(update, context):
     current_player = player_2 if current_player == player_1 else player_1
 
 
-# while candy_count > max_candy_takes:
-#     print(f'\nНа столе осталось {candy_count} конфет...')
-#     while True:
-#         if current_player == player_1:
-#             try:
-#                 take_candies = int(input(f'Сколько конфет берёт {current_player} (1-{max_candy_takes}): '))
-#             except ValueError:
-#                 print('Введите число')
-#         # компьютер прикидывается дурачком и предпоследним ходом не оставляет шансов игроку победить
-#         else:
-#             if (max_candy_takes + 1) < candy_count <= (max_candy_takes * 2 + 1):
-#                 take_candies = abs(max_candy_takes + 1 - candy_count)
-#             else:
-#                 take_candies = randint(1, max_candy_takes)
-#             print(f'{current_player} берёт {take_candies} конфет')
-#         if 1 <= take_candies <= max_candy_takes:  # проверка, что игрок взял разрешенное количество конфет
-#             break
-#     candy_count -= take_candies
-#     current_player = player_2 if current_player == player_1 else player_1
-# print(f'{current_player} забрал последние {candy_count} конфет и ПОБЕДИЛ!')
-# print(f'{current_player} получает все конфеты {player_2 if current_player == player_1 else player_1}')
-
-
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
         PLAYER: [MessageHandler(Filters.text, player_name)],
         MOVE: [MessageHandler(Filters.text, take_move)],
-        # IS_DONE: [MessageHandler(Filters.text, edit_f)]
     },
     fallbacks=[CommandHandler('cancel', start)]
 )
